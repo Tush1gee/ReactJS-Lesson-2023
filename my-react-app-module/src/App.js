6b35aff6c139dd05d58f7cd855b1129dd899ac2b
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-02 12:53:30 +0800

    Document Product Completed

--- my-react-app-module/src/App.js
+++ my-react-app-module/src/App.js
@@ -3,22 +3,63 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header className="app-header">
+        <h1>Popular Products</h1>
+        <hr></hr> 
+        <div className="select-first">
+          <img className="pictureyellow" src="image-yellow.png"></img>
+          <div className="all-text">
+            <h6> ⏏️55 </h6>
+            <a href="#">Haught or Naught</a>
+            <p>High-minded or absent-minded? You decide.</p>
+            <div className="submitted">
+              <p>Submitted by:</p>
+              <img src="famale.png"></img>
+            </div>
+          </div>
+        </div>
+
+        <div className="select-first">
+          <img className="pictureyellow" src="image-aqua.png"></img>
+          <div className="all-text">
+            <h6> ⏏ 44</h6>
+            <a href="#">Yellow Pail</a>
+            <p>On-demand sand castle construction expertise.</p>
+            <div className="submitted">
+              <p>Submitted by:</p>
+              <img src="profile.png"></img>
+            </div>
+          </div>
+        </div>
+
+        <div className="select-first">
+          <img className="pictureyellow" src="image-steel.png"></img>
+          <div className="all-text">
+            <h6> ⏏️ 42</h6>
+            <a href="#">Tinfoild: Tailored tinfoil hats</a>
+            <p>We already have your measurements and shipping adress.</p>
+            <div className="submitted">
+              <p>Submitted by:</p>
+              <img src="famale.png"></img>
+            </div>
+          </div>
+        </div>
+
+        <div className="select-first">
+          <img className="pictureyellow" src="image-rose.png"></img>
+          <div className="all-text">
+            <h6> ⏏️ 23</h6>
+            <a href="#">Supermajority: The Fantasy Congress League</a>
+            <p>earn point when you favorite politicians pass legislation.</p>
+            <div className="submitted">
+              <p>Submitted by:</p>
+              <img src="profile.png"></img>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+    </body>
   );
 }
 
