6f7a888419e84ca87de57393ce826e2be5b28651
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-09 11:35:09 +0800

    handler used

--- my-react-app-module/src/Product.jsx
+++ my-react-app-module/src/Product.jsx
@@ -1,11 +1,20 @@
 // import React from "react"
+function handleUpVote(props){
+  console.log('upvoted')
+  console.log(props.id)
+}
+
 
 function ProductFunc(props){
     return (
       <div className="select-first">
         <img src={props.productImageUrl}></img>
         <div className="all-text">
-          <h6><i class="fa-solid fa-caret-up">{props.id}</i></h6>
+            <a onClick={() => {handleUpVote(props)}}>
+              <h6>
+                <i class="fa-solid fa-caret-up">{props.id}</i>
+              </h6>
+            </a>
           <a>{props.title}</a>
           <p>{props.description}</p>
           <div className="submitted"><p>Submitted by:</p><img src={props.submitterAvatarUrl}></img></div>
