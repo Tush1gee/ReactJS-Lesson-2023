9cfa86d8ff660c499fa48154ec6e0c29c452c091
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-12 11:54:50 +0800

    react array

---
+++ day-41/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useState } from 'react'
+import UpdateArrays from './components/updateArrays';
+import List from './components/SpreadArray';
+import UpdatingObject from './components/UpdatingObject';
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+  const [inputText, setInputText] = useState("")
+
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+
+  }
+
+
+  const handleInput = function(event){
+    console.log(event.target.value)
+    console.log('handle input clicket')
+    setInputText(event.target.value)
+  }
+  
+
+  return (
+    <div className="App">
+      <UpdatingObject />
+      {/* <List/> */}
+      {/* <UpdateArrays /> */}
+      {/* <div className='ex1'>
+        <p>{counter}</p>
+        <button onClick={ handleClick }>click</button>
+      </div>
+
+      <div className='ex2'>
+        <p>{inputText}</p>
+        <input value={""} onChange={(e) => {handleInput()}} />
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
