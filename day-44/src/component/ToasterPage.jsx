b4dfd89bebac580d3fa37ff1f6b3d21525307e69
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-17 12:19:47 +0800

    text changing

--- day-44/src/component/ToasterPage.jsx
+++ day-44/src/component/ToasterPage.jsx
@@ -1,7 +1,7 @@
 import { toast, ToastContainer } from "react-toastify"
 
 export default function ToasterPage (){
-    const notify = () => toast("Amjilttai hadgalagdlaa. !");
+    const notify = () => toast("Амжилттай хадгалагдлаа ! ✅");
     return (
         <div className="toast-area">
             <button onClick={notify}>Хадгалах</button>
