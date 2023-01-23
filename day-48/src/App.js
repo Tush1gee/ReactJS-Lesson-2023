aa9917c90cbab6859e957e6605f97041d684ef04
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-23 11:09:19 +0800

    code zasaw

--- day-48/src/App.js
+++ day-48/src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Input from "./components/Input";
-import { useState } from "react";
-import Anime from "./components/Anime";
-import Button from "./components/Button";
-import TopAnime from "./components/TopAnime";
+import { useState } from 'react';
+import Anime from './components/Anime';
+import Button from './components/Button';
+import Input from './components/Input';
+import TopAnime from './components/TopAnime';
 
-function App() {
-  const [text, setText] = useState("");
+export default function App() {
+  const [text, setText] = useState('');
 
   function handleChange(e) {
     setText(e.target.value);
@@ -15,14 +13,19 @@ function App() {
 
   return (
     <div>
-      <h1>DAY-48</h1>
-      <Input label="First input" value={text} onChange={handleChange} />
-      <Input label="Second input" value={text} onChange={handleChange} />
-      <Anime/>
-      <Button/>
-      <TopAnime/>
+      <Input
+        label="First input"
+        value={text}
+        onChange={handleChange}
+      />
+      <Input
+        label="Second input"
+        value={text}
+        onChange={handleChange}
+      />
+      <Anime />
+      <Button />
+      <TopAnime />
     </div>
   );
 }
-
-export default App;
