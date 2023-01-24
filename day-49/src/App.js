209b79ed5da11b852c70be7e9c83e47559499eb2
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-24 09:47:05 +0800

    use fancy styling

--- day-49/src/App.js
+++ day-49/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+  const [isFancy, setIsFancy] = useState(false);
   return (
     <div>
-
+      <Counter isFancy={isFancy} />
+      <label>
+        <input
+          type="checkbox"
+          checked={isFancy}
+          onChange={(e) => {
+            setIsFancy(e.target.checked);
+          }}
+        />
+        USe fancy styling
+      </label>
     </div>
   );
 }
