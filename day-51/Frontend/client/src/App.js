b2565a035381f15e462ab799b0a2b029ca6f9e0d
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-27 09:47:38 +0800

    edit form use

--- day-51/Frontend/client/src/App.js
+++ day-51/Frontend/client/src/App.js
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 import InputForm from "./components/InputForm";
+import UpdateForm from "./components/UpdateForm";
 
 const GET_DATA_URL = "http://localhost:8080/data";
 const DELETE_DATA_URL = "http://localhost:8080/data";
@@ -9,6 +9,9 @@ const DELETE_DATA_URL = "http://localhost:8080/data";
 function App() {
   const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isOpenForm, setIsOpenForm] = useState(false);
+  const [currentData, setCurrentData] = useState({});
+
 
   useEffect(() => {
     fetchData();
@@ -21,39 +24,59 @@ function App() {
     setData(data); // avsan dataga dotor n hiij bga
   }
 
-  async function deleteData(data){
+  async function deleteData(data) {
     const options = {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data),
+    };
     const FETCHED_DELETE_DATA = await fetch(DELETE_DATA_URL, options);
     const FETCHED_DATA = await FETCHED_DELETE_DATA.json();
     setData(FETCHED_DATA);
   }
 
-  function handleDelete (id) {
+  function handleDelete(id) {
     const data = {
       id: id,
-    }
+    };
     deleteData(data);
+  }
+
+  function handleEdit(data) {
+    setCurrentData(data);
+    setIsOpenForm(true);
   }
 
   return (
     <div>
       <h1>DAY - 51 React/Express FullStack APP - without Database</h1>
       {/* door input form maani propsor avj bga */}
-      <InputForm isLoaded={isLoaded} setIsLoaded={setIsLoaded} setData={setData} />
+      <InputForm
+        isLoaded={isLoaded}
+        setIsLoaded={setIsLoaded}
+        setData={setData}
+      />
+      {isOpenForm ? (
+        <UpdateForm
+         setCurrentData={setCurrentData}
+         currentData={currentData}
+        />
+      ) : (
+        <div></div>
+      )}
       {isLoaded
         ? "...Уншиж байна"
         : data &&
           data.map((d, index) => {
             return (
               <div key={index}>
-                <p>{d.name} -- {d.age}</p>
+                <p>
+                  {d.name} -- {d.age}
+                </p>
                 <button onClick={() => handleDelete(d.id)}>Delete</button>
+                <button onClick={() => handleEdit(d)}>Edit</button>
               </div>
             );
           })}
