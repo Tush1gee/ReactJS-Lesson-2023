b3bdf56ed254320d4aa111865b8f304533770c60
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-30 10:55:56 +0800

    user data saved backend

---
+++ day-52/fe/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  const URL = "http://localhost:8080/users";
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const postData = {
+      username: e.target.username.value,
+      age: e.target.age.value,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    };
+
+    const FETCHED_DATA = await fetch(URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    console.log(FETCHED_JSON);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          User Name:
+          <input name="username" />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input name="age" />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
