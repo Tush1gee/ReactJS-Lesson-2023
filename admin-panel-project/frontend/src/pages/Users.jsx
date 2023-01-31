6b4e196d6122a59cbb5ea7c7542b46cd6170907b
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-31 11:40:13 +0800

    Admin-Panel-Project

---
+++ admin-panel-project/frontend/src/pages/Users.jsx
@@ -0,0 +1,10 @@
+import UsersTable from "../components/UsersTable";
+
+export default function Users () {
+    return (
+        <div>
+            <h3>Users page</h3>
+            <UsersTable/>
+        </div>
+    )
+}
