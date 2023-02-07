c6f52a0f679240f577df2a5cfc591e52b06e9207
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-02-07 12:22:27 +0800

    React Context

---
+++ day-57-context/context-lesson/src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LevelContext } from "../context/LevelContext";
+
+export default function Section({level, children}) {
+
+  return (
+    <section className="section">
+        <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    </section>
+  )
+}
---
+++ day-57-context/context-lesson/src/data/data.js
@@ -0,0 +1,12 @@
+export const DATA = [
+  {
+    id: 1,
+    title: "the Road to React",
+    price: 25000,
+  },
+  {
+    id: 2,
+    title: "Fillstack React",
+    price: 30000,
+  },
+];
