a1279dc9a8a49a8dbaa609a033df0db456a2ae7b
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-02-14 12:09:36 +0800

    navigate, role permission

--- day-62-user-crud-admin/backend/index.js
+++ day-62-user-crud-admin/backend/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const { request } = require("http");
-const { response } = require("express");
 
 const app = express();
 PORT = 8080;
@@ -26,24 +24,40 @@ app.post("/register", (request, response) => {
 
     const readDataObj = JSON.parse(readData);
     console.log(readDataObj);
-    readDataObj.push(body); // hooson array hiij chadhar bolson
 
-    //fs write
-    fs.writeFile(
-      "./data/users.json",
-      JSON.stringify(readDataObj),
-      (writeError) => {
-        if (writeError) {
-          response.json({
-            status: "file write error",
-          });
-        }
+    fs.readFile("./data/userrole.json", "utf-8", (readError, readData) => {
+      if (readError) {
         response.json({
-          status: "Amjilttai",
-          data: readDataObj,
+          status: "file unshihad aldaa garlaa !",
+          data: [],
         });
       }
-    );
+      const roleData = JSON.parse(readData); // parse ashiglan object bolgoj bga
+      const roleName = roleData.filter((role) => role.id === body.role)[0];
+
+      const userData = {
+        ...body,
+        role: roleName,
+      };
+
+      readDataObj.push(userData); // hooson array hiij chadhar bolson
+      //fs write
+      fs.writeFile(
+        "./data/users.json",
+        JSON.stringify(readDataObj),
+        (writeError) => {
+          if (writeError) {
+            response.json({
+              status: "Aldaa garlaa !!! JSON file zuruud bn",
+            });
+          }
+          response.json({
+            status: "Amjilttai",
+            data: readDataObj,
+          });
+        }
+      );
+    });
   });
 });
 
@@ -57,6 +71,21 @@ app.get("/users", (request, response) => {
       });
     }
 
+    response.json({
+      status: "Amjilttai",
+      data: JSON.parse(readData),
+    });
+  });
+});
+
+// API get all user ROLES
+app.get("/users/roles", (request, response) => {
+  fs.readFile("./data/userrole.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "ymr negen alda garlaa!!!",
+      });
+    }
     response.json({
       status: "Amjilttai",
       data: JSON.parse(readData),
--- day-62-user-crud-admin/frontend/src/components/UserRegisterForm.jsx
+++ day-62-user-crud-admin/frontend/src/components/UserRegisterForm.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import TextField from '@mui/material/TextField'
-import { Grid } from "@mui/material";
+import { Grid, InputLabel, MenuItem, Select } from "@mui/material";
 import { Button } from "@mui/material";
+import {useNavigate} from 'react-router-dom'
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function UserRegisterForm() {
     const URL = "http://localhost:8080/register"
+    const ROLE_URL = "http://localhost:8080/users/roles"
 
+    const [roles, setRoles] = useState([])
+    const [currentRole, setCurrentRole] = useState(0)
+
+    useEffect(()=> {
+        fetchRoles()
+    }, [])
+
+    async function fetchRoles () {
+        const FETCHED_DATA = await fetch(ROLE_URL)
+        const FETCHED_JSON = await FETCHED_DATA.json()
+        setRoles(FETCHED_JSON.data)
+        console.log(roles);
+    }
+
+    function handleSelectChange(e) {
+        console.log(e.target.value);
+        setCurrentRole(e.target.value);
+    }
+
+    const navigate = useNavigate();  // shuud navigate aar register page luu orno
     const handleSubmit = async (e) => {
         e.preventDefault()
         console.log(e.target);
@@ -15,6 +39,7 @@ export default function UserRegisterForm() {
             email: e.target.email.value,
             password: e.target.password.value,
             address: e.target.address.value,
+            role: currentRole
         }
 
         const options = {
@@ -28,6 +53,10 @@ export default function UserRegisterForm() {
         const FETCHED_DATA = await fetch(URL, options)
         const FETCHED_JSON = await FETCHED_DATA.json()
         console.log(FETCHED_JSON.data);
+
+        if(FETCHED_JSON) {
+            navigate('/users')
+        }
     }
   return (
     <div>
@@ -86,6 +115,18 @@ export default function UserRegisterForm() {
                         size='small'
                         defaultValue=""
                         label="Address"/>
+                </Grid>
+                <Grid item xs={12}>
+                    <InputLabel>User Roles</InputLabel>
+                    <Select id="role-selector" value={currentRole} label="Roles" onChange={handleSelectChange}>
+                       {
+                        roles && roles.map((role, khuslen) => {
+                            return (
+                                <MenuItem key={khuslen} value={role.id}>{role.name}</MenuItem>
+                            )
+                        })
+                       }
+                    </Select>
                 </Grid>
                 <Grid item xs={12}>
                     <Button type="submit" variant="outlined">Submit</Button>
