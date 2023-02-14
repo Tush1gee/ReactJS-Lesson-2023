ca848d6e86dbae28dd81693fc8fb2c8442883d2a
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-02-14 10:23:25 +0800

    page posted user data

--- day-62-user-crud-admin/frontend/src/App.js
+++ day-62-user-crud-admin/frontend/src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import UserRegisterForm from "./components/UserRegisterForm";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <h1> DAY - 62 USER CRUD ADMIN</h1>
       <h5>USER REGISTER FORM</h5>
-      <UserRegisterForm />
+      <Routes>
+        <Route path="/register" element={<UserRegisterForm />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
