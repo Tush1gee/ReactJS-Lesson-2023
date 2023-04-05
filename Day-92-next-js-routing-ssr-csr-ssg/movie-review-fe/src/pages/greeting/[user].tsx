d1ece6ae09fb41d194bfafcef22fc3343f9ad0ed
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-04-05 10:17:20 +0800

    query param

--- Day-92-next-js-routing-ssr-csr-ssg/movie-review-fe/src/pages/greeting/[user].tsx
+++ Day-92-next-js-routing-ssr-csr-ssg/movie-review-fe/src/pages/greeting/[user].tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(req: any) {
     const {user} = req.params; //request name params aara avj bga
@@ -12,9 +13,12 @@ export async function getServerSideProps(req: any) {
 
 
 export default function Greeting(props: any): JSX.Element {
+    const {query} = useRouter()
+    console.log(query) 
   return (
     <div>
         <h1>Hello {props.user}</h1>
+        <h3>He is {query.age} years old</h3>
     </div>
   )
 }
