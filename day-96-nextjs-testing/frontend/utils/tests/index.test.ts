05cb7f42ee15d8d0405c6a127b40e71697433505
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-04-11 10:26:36 +0800

    jest test

---
+++ day-96-nextjs-testing/frontend/utils/tests/index.test.ts
@@ -0,0 +1,39 @@
+import { cutTextToLength, slugify, composeArticleSlug, extractArticleIdFromSlug } from "../index";
+
+// const str = "The quick brown fox jumps over the lazy dog.";
+
+// describe("cutTextToLength", () => {
+//   it("should cut text to length", () => {
+//     expect(cutTextToLength(str, 20)).toBe("The quick brown fox ...");
+//   });
+
+//   it("should not cut text to length", () => {
+//     expect(cutTextToLength(str, 100)).toBe(str);
+//   });
+// });
+
+// const str = "tushig0721--=+[]/.@gmail.com"
+
+// describe("slugify", () => {
+//     it("", () => {
+//       expect("tushig0721gmailcom");
+//     });
+  
+//     it("", () => {
+//       expect(slugify(str));
+//     });
+//   });
+
+describe("composeArticle", () => {
+    
+  it("", () => {
+    expect(composeArticleSlug(123, "person")).toBe("person-123"); //Expected-хүлээгдэж байгаа: "-20" 
+  });                                                             //Received-хүлээн авсан нь "person-123"
+
+  it("", () => {
+    expect(extractArticleIdFromSlug("person-123")).toBe("123"); //Expected-хүлээгдэж байгаа: ""
+  });                                                           //Received-хүлээн авсан нь "123"
+  
+});
+ 
+
