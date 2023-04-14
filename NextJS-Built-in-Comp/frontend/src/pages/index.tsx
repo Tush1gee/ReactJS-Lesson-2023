f3884b9e797cd55c6bc74736e010706ecdfb3a9c
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-04-14 08:17:57 +0800

    dawtalt

---
+++ NextJS-Built-in-Comp/frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function IndexPage(): JSX.Element {
+
+  return (
+    <div>
+     <Link href="/posts/navbar">Navbar</Link>
+    </div>
+  )
+}
