9b9c5b2fb91e276bb24c37e4117a4993c9dde35d
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-04-27 11:47:15 +0800

    github profile image, username

--- day-102-oauth-with-nextauth/frontend/src/components/header.tsx
+++ day-102-oauth-with-nextauth/frontend/src/components/header.tsx
@@ -1,25 +1,68 @@
-import { useSession, signIn } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import styles from "../styles/header.module.css";
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   const handleLogin = (e: any) => {
     e.preventDefault();
     signIn();
   };
+
+  const handleLogout = (e: any) => {
+    e.prevent.Default();
+    signOut();
+  };
+
   return (
     <header>
-      <div>
+      <noscript>
+        <style>{`.nojs-show {opacity: 1; top;}`}</style>
+      </noscript>
+      <div className={styles.signedInStatus}>
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.load
+          }`}
+        ></p>
         <div>
           {!session && (
             <div>
               <span>You are not signed in</span>
               <br />
-              <a href={`/api/auth/signin`} onClick={handleLogin} className=" bg-green-400 py-2 px-6 rounded-3xl">
+              <a
+                href={`/api/auth/signin`}
+                onClick={handleLogin}
+                className=" bg-green-400 py-2 px-6 rounded-3xl"
+              >
                 Sign In
               </a>
             </div>
           )}
+
+          {session?.user && (
+            <div>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url(${session.user.image})` }}
+                  className={styles.avatar}
+                ></span>
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email || session.user.name}</strong>
+              </span>
+            </div>
+          )}
+          <a
+            href={`/api/auth/signout`}
+            className={styles.button}
+            onClick={handleLogin}
+          >
+            Sign out
+          </a>
         </div>
       </div>
     </header>
--- day-102-oauth-with-nextauth/frontend/src/pages/api/auth/[...nextauth].ts
+++ day-102-oauth-with-nextauth/frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
+import FacebookProvider from "next-auth/providers/facebook"
+import InstagramProvider from "next-auth/providers/instagram"
+import GmailProvider from "next-auth/providers/google"
+import TwitchProvider from "next-auth/providers/twitch"
+import TwitterProvider from "next-auth/providers/twitter"
 
 
 export const authOptions: NextAuthOptions = {
@@ -7,6 +12,28 @@ export const authOptions: NextAuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+        }),
+        InstagramProvider({
+            clientId: process.env.INSTAGRAM_ID,
+            clientSecret: process.env.INSTAGRAM_SECRET,
+        }),
+        GmailProvider({
+            clientId: process.env.GMAIL_ID,
+            clientSecret: process.env.GMAIL_SECRET,
+        }),
+
+        TwitchProvider({
+            clientId: process.env.TWITCH_ID,
+            clientSecret: process.env.TWITCH_SECRET,
+        }),
+
+        TwitterProvider({
+            clientId: process.env.TWITCH_ID,
+            clientSecret: process.env.TWITCH_SECRET,
         }),
     ],
 };
